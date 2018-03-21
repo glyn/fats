32036912ddad818fb1db1b541b76456064e57673
Scott Andrews <sandrews@pivotal.io>
2018-03-21 12:06:50 -0400

    Collect all riff-system logs

--- functions/uppercase/run.sh
+++ functions/uppercase/run.sh
@@ -18,8 +18,8 @@ for invoker in command go java node python3; do
     kail --label "function=$function_name" > $function_name.logs &
     kail_function_pid=$!
 
-    kail --label app=riff --ns riff-system > $function_name.controller.logs &
-    kail_controller_pid=$!
+    kail --ns riff-system > $function_name.system.logs &
+    kail_system_pid=$!
 
     riff create $args \
       --useraccount $useraccount \
@@ -36,7 +36,7 @@ for invoker in command go java node python3; do
     expected_data="HELLO"
     actual_data=`cat $function_name.out | tail -1`
 
-    kill $kail_function_pid $kail_controller_pid
+    kill $kail_function_pid $kail_system_pid
     riff delete --all --name $function_name
     gcloud container images delete "${useraccount}/${function_name}:${function_version}"
 
@@ -44,8 +44,8 @@ for invoker in command go java node python3; do
       echo -e "Function Logs:"
       cat $function_name.logs
       echo -e ""
-      echo -e "Controller Logs:"
-      cat $function_name.controller.logs
+      echo -e "System Logs:"
+      cat $function_name.system.logs
       echo -e ""
       echo -e "${RED}Function did not produce expected result${NC}";
       echo -e "   expected: $expected_data"
